16803715b8241e597b9644d03fe0bc8957e7724a
[David <[nmueller855@gmail.com]>
2023-01-23 16:43:10 -0800

    made api call to git hub

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import './App.css';
 import useFetch from './useFetch';
 
 function App() {
-  const { data, loading, error } = useFetch('https://v2.jokeapi.dev/joke/Any');
+  // const { data, loading, error } = useFetch(' https://v2.jokeapi.dev/joke/Any');
+  const { data, loading, error } = useFetch(
+    ' https://api.github.com/users/d-pagey'
+  );
   if (loading) return <h1> Loading ...</h1>;
   if (error) return console.log(error);
   return (
     <div className='App'>
       <h1>
-        {data?.setup} : {data?.delivery}
+        {data?.url} : {data?.login}
       </h1>
     </div>
   );
