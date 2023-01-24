4909fb79e0a99b89c1577a3900c09e465c33e28c
[David <[nmueller855@gmail.com]>
2023-01-24 10:13:48 -0800

    Separate component file for createUser

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import './App.css';
 import useFetch from './useFetch';
+import CreateUser from './CreateUser';
 
 function App() {
-  // const { data, loading, error } = useFetch(' https://v2.jokeapi.dev/joke/Any');
-  const { data, loading, error } = useFetch(
-    ' https://api.github.com/users/d-pagey'
-  );
-  if (loading) return <h1> Loading ...</h1>;
-  if (error) return console.log(error);
-
   return (
     <div className='App'>
-      <div>
-        <p>
-          <img src={data?.avatar_url} alt={data?.name} />
-        </p>
-        <p>Login: {data?.login}</p>
-        <p>
-          GitHub Profile:{' '}
-          <a href={data?.html_url} target='_blank'>
-            {data?.login}
-          </a>
-        </p>
-      </div>
+      <CreateUser />
     </div>
   );
 }
--- src/createUser.js
+++ src/createUser.js
@@ -1,1 +1,25 @@
 import useFetch from './useFetch';
+
+export default function CreateUser() {
+  const { data, loading, error } = useFetch(
+    ' https://api.github.com/users/d-pagey'
+  );
+  if (loading) return <h1> Loading ...</h1>;
+  if (error) return console.log(error);
+
+  return (
+    <div className='user'>
+      <p>
+        <img src={data?.avatar_url} alt={data?.name} />
+      </p>
+      <p>Login: {data?.login}</p>
+      <p>
+        GitHub Profile:{' '}
+        <a href={data?.html_url} target='_blank'>
+          {data?.login}
+        </a>
+      </p>
+      >
+    </div>
+  );
+}
