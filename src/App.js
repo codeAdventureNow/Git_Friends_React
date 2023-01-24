a1600b636b3e7dd3de5ef557f01bc2daa70dc7b6
[David <[nmueller855@gmail.com]>
2023-01-24 10:39:17 -0800

    input and search button up, style connected, button is not functional yet

--- src/App.js
+++ src/App.js
@@ -2,12 +2,24 @@ import './App.css';
 import useFetch from './useFetch';
 import CreateUser from './CreateUser';
 
-function App() {
+export default function App() {
   return (
     <div className='App'>
+      <h1>
+        Git <span className='Friends'>Friends</span>
+      </h1>
+      <div className='button'>
+        <button id='fetchdata'>Search</button>
+      </div>
+      <div>
+        <input
+          className='inputContainer'
+          placeholder='    Enter Git Hub Name'
+          type='text'
+          id='fetchdata'
+        />
+      </div>
       <CreateUser />
     </div>
   );
 }
-
-export default App;
