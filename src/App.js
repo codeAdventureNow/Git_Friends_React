516e81cf6e02bbb71294764b046d9268cc5a8f73
[David <[nmueller855@gmail.com]>
2023-01-24 10:05:48 -0800

    We have profile up in root app, unstyled

--- src/App.js
+++ src/App.js
@@ -8,11 +8,21 @@ function App() {
   );
   if (loading) return <h1> Loading ...</h1>;
   if (error) return console.log(error);
+
   return (
     <div className='App'>
-      <h1>
-        {data?.url} : {data?.login}
-      </h1>
+      <div>
+        <p>
+          <img src={data?.avatar_url} alt={data?.name} />
+        </p>
+        <p>Login: {data?.login}</p>
+        <p>
+          GitHub Profile:{' '}
+          <a href={data?.html_url} target='_blank'>
+            {data?.login}
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
